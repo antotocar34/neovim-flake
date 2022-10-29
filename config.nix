bf1e03e2dc3bdb4b9283680d32065a41ef751b42
Antoine Carnec <antoinecarnec@gmail.com>
2022-10-29 19:01:59 +0100

    Put module configs in config.nix

---
+++ config.nix
@@ -0,0 +1,75 @@
+{ isMaximal }:
+
+{
+        theme = {
+          enable = true;
+          name = "nord";
+        };
+        viAlias = false;
+        vimAlias = true;
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          nvimCodeActionMenu.enable = true;
+          trouble.enable = true;
+          lspSignature.enable = true;
+          nix = true;
+          python = true;
+          clang.enable = false;
+          sql = true;
+          ts = false;
+          go = false;
+          hare = false;
+        };
+        visuals = {
+          enable = true;
+          nvimWebDevicons.enable = true;
+          lspkind.enable = true;
+          indentBlankline = {
+            enable = true;
+            fillChar = "";
+            eolChar = "";
+            showCurrContext = true;
+          };
+          cursorWordline = {
+            enable = true;
+            lineTimeout = 0;
+          };
+        };
+        statusline.lualine = {
+          enable = true;
+          theme = "nord";
+        };
+        autopairs.enable = true;
+        autocomplete = {
+          enable = true;
+          type = "nvim-cmp";
+        };
+        filetree.nvimTreeLua = {
+          enable = true ;
+          closeOnFileOpen = true ;
+          openOnSetup = false;
+        };
+        tabline.nvimBufferline.enable = false;
+        treesitter = {
+          enable = true;
+          context.enable = true;
+        };
+        keys = {
+          enable = true;
+          whichKey.enable = true;
+        };
+        telescope = {
+          enable = true;
+        };
+        markdown = {
+          enable = true;
+          glow.enable = true;
+        };
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+        };
+  }
--- flake.nix
+++ flake.nix
@@ -254,7 +254,6 @@
       "nvim-autopairs"
       "nvim-ts-autotag"
       "nvim-web-devicons"
-      "tokyonight"
       "bufdelete-nvim"
       "nvim-cmp"
       "cmp-nvim-lsp"
@@ -276,6 +275,8 @@
       "rust-tools"
       "onedark"
       "hare-vim"
+      "nord"
+      "tokyonight"
     ];
 
     pluginOverlay = lib.buildPluginOverlay;
@@ -307,77 +308,7 @@
     };
 
     configBuilder = isMaximal: {
-      config = {
-        vim.viAlias = false;
-        vim.vimAlias = true;
-        vim.lsp = {
-          enable = true;
-          formatOnSave = true;
-          lightbulb.enable = true;
-          lspsaga.enable = false;
-          nvimCodeActionMenu.enable = true;
-          trouble.enable = true;
-          lspSignature.enable = true;
-          nix = true;
-          rust.enable = isMaximal;
-          python = isMaximal;
-          clang.enable = isMaximal;
-          sql = isMaximal;
-          ts = isMaximal;
-          go = isMaximal;
-          hare = isMaximal;
-        };
-        vim.visuals = {
-          enable = true;
-          nvimWebDevicons.enable = true;
-          lspkind.enable = true;
-          indentBlankline = {
-            enable = true;
-            fillChar = "";
-            eolChar = "";
-            showCurrContext = true;
-          };
-          cursorWordline = {
-            enable = true;
-            lineTimeout = 0;
-          };
-        };
-        vim.statusline.lualine = {
-          enable = true;
-          theme = "onedark";
-        };
-        vim.theme = {
-          enable = true;
-          name = "onedark";
-          style = "darker";
-        };
-        vim.autopairs.enable = true;
-        vim.autocomplete = {
-          enable = true;
-          type = "nvim-cmp";
-        };
-        vim.filetree.nvimTreeLua.enable = true;
-        vim.tabline.nvimBufferline.enable = true;
-        vim.treesitter = {
-          enable = true;
-          context.enable = true;
-        };
-        vim.keys = {
-          enable = true;
-          whichKey.enable = true;
-        };
-        vim.telescope = {
-          enable = true;
-        };
-        vim.markdown = {
-          enable = true;
-          glow.enable = true;
-        };
-        vim.git = {
-          enable = true;
-          gitsigns.enable = true;
-        };
-      };
+      config.vim = import ./config.nix { inherit isMaximal; };
     };
   in rec {
     apps.${system} = rec {
