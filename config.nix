31eae4c2fdb5fa42ccc1253e9b0193db936eacef
Antoine Carnec <antoinecarnec@gmail.com>
2022-11-07 12:05:37 +0000

    Added highlight yank

--- config.nix
+++ config.nix
@@ -116,4 +116,11 @@
         nnoremap."<leader>q"=":bd<CR>";
 
         imap."<C-e>"="<C-o>zz";
+
+        configRC = ''
+            augroup highlight_yank
+             autocmd!
+             au TextYankPost * silent! lua vim.highlight.on_yank{timeout=200}
+            augroup END
+          '';
   }
